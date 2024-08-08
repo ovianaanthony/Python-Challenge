da197e2a232efffc0b152bcd713a4eabf7f2a0c0
ovianaanthony <anthony.pe.viana@gmail.com>
2024-08-08 15:54:14 -0300

    projetcs created

--- desafio2.py
+++ desafio2.py
@@ -1,4 +1,8 @@
-conta = {
+usuarios = []      # nome, data de nascença, cpf, endereco
+conta = []         # numero de conta, agencia, usuarios
+AGENCIA = "0001"
+
+controle = {
     'saldo': 0,
     'limit': 0,
     'acoes': 0
@@ -50,6 +54,42 @@ def tirar_extrato():
     print(f'\nO saldo da sua conta é R${conta['saldo']:.2f}.\n')
     menu()
 
+def criar_usuario(usuarios):
+    cpf = input("Digite o seu cpf: ")
+    boleano = filtrar_user(cpf, usuarios)
+
+    if (boleano):
+        print("Usuário já existente no sistema")
+        menu()
+
+    nome = input("Digite seu nome: ")
+    data_nascimento = input("Digite sua data de nascimento: ")
+    endereco = input("Digite seu endereço: ")
+    usuarios.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereco": endereco})
+    print("\nUsuário criado com sucesso!")
+    menu()
+
+
+def criar_conta(agencia, numero_conta, usuarios):
+    cpf = input("Digite o seu cpf: ")
+    boleano = filtrar_user(cpf, usuarios)
+
+    if(boleano):
+        print("Conta criada com sucesso!")
+        return conta.append({"agencia": agencia, "numero_conta": numero_conta, "usuario": boleano})
+    print("Usuário não encontrado!")
+    menu()
+
+def filtrar_user(cpf, usuarios):
+    usuarios_filtrados = []
+    for usuario in usuarios: 
+        if (usuario["cpf"]==cpf):
+            usuarios_filtrados.append(usuario)
+    if(usuarios_filtrados):
+        return usuarios_filtrados[0]
+    else:
+        return None
+
 def menu():
     print("""
     ================ MENU ================
@@ -59,6 +99,8 @@ def menu():
     1 - Depósito
     2 - Saque 
     3 - Extrato
+    4 - Criar novo usuário
+    5 - Criar nova conta
     0 - Encerrar movimentação
       
     ======================================    
@@ -78,6 +120,13 @@ def menu():
         sacar(quantidade=quanti)
     elif(escolha == "3"):
         tirar_extrato()
+    elif(escolha == "4"):
+        criar_usuario(usuarios)
+    elif(escolha == "5"):
+        numero_conta = len(conta)+1
+        conta_var = criar_conta(AGENCIA, numero_conta, usuarios)
+        if(conta_var):
+            conta.append(conta_var)
     else:
         print("Opção inválida tente novamente.")
         menu()
